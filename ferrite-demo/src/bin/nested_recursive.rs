c58eccf8acf755d6572fce0901362ebe15bdeaeb
Soares Chen <soares.chen@gmail.com>
2021-05-13 11:37:50 +0200

    Nested recursive session types demo is now working

--- ferrite-demo/src/bin/nested_recursive.rs
+++ ferrite-demo/src/bin/nested_recursive.rs
@@ -3,34 +3,87 @@ use ferrite_session::{
   prelude::*,
 };
 
-pub type Stream = Rec<
-  InternalChoice<
+type Stream = Rec<
+  ExternalChoice<
     Either<
       Rec<InternalChoice<Either<SendValue<String, Z>, S<Z>>>>,
-      ReceiveValue<String, Z>,
+      ReceiveValue<String, End>,
     >,
   >,
 >;
 
-pub fn stream_producer() -> Session<Stream>
+fn producer() -> Session<Stream>
 {
-  fix_session(offer_case!(
-    Left,
-    fix_session(offer_case!(
-      Left,
-      send_value(
-        "foo".to_string(),
-        fix_session(offer_case!(
+  fix_session(offer_choice! {
+    Left =>
+      fix_session(offer_case!(
+        Left,
+        send_value(
+          "foo".to_string(),
+          fix_session(offer_case!(
+            Left,
+            send_value(
+              "bar".to_string(),
+              fix_session(offer_case!(Right, producer()))
+            )
+          ))
+        )))
+    Right =>
+      receive_value(| str | {
+        println!("[producer] received string: {}", str);
+        terminate()
+      })
+  })
+}
+
+fn consume_input() -> Session<
+  ReceiveChannel<
+    RecX<HList![Stream], InternalChoice<Either<SendValue<String, Z>, S<Z>>>>,
+    End,
+  >,
+>
+{
+  receive_channel(|chan| {
+    unfix_session_for(
+      chan,
+      case! { chan;
+        Left =>
+          receive_value_from(chan, move | val | {
+            println!("[consumer] received value: {}", val);
+            include_session(consume_input(), | consume |
+              send_channel_to(consume, chan,
+                wait(consume, terminate())))
+          }),
+        Right =>
+          unfix_session_for(chan,
+            choose!(chan, Right,
+              send_value_to(chan,
+                "hello".to_string(),
+                wait(chan,
+                  terminate()))))
+      },
+    )
+  })
+}
+
+fn consumer() -> Session<ReceiveChannel<Stream, End>>
+{
+  include_session(consume_input(), |consume| {
+    receive_channel(|chan| {
+      unfix_session_for(
+        chan,
+        choose!(
+          chan,
           Left,
-          send_value(
-            "bar".to_string(),
-            fix_session(offer_case!(Right, stream_producer()))
-          )
-        ))
+          send_channel_to(consume, chan, wait(consume, terminate()))
+        ),
       )
-    ))
-  ))
+    })
+  })
 }
 
 #[tokio::main]
-pub async fn main() {}
+pub async fn main()
+{
+  run_session(apply_channel(consumer(), producer())).await
+}
